c2abe35a90fefa07d54a6f9ab84731a70564cfc1
nggadev <nggadev@gmail.com>
2021-09-21 14:42:30 +0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,18 @@
 // fs.copyFileSync("text.txt", "text2.txt");
 // console.log('text.txt was copied to text2.txt');
 
-const hero = require("superheroes");
-for (let i = 0; i < 10; i++) {
-    console.log(hero.random());
-}
+// const hero = require("superheroes");
+// for (let i = 0; i < 10; i++) {
+//     console.log(hero.random());
+// }
+
+
+const op = require("./module");
+
+const moduleTitle = op.title;
+const moduleAdd = op.tambah(10, 5);
+const moduleTimes = op.kali(10, 5);
+
+console.log(moduleTitle);
+console.log(moduleAdd);
+console.log(moduleTimes);
